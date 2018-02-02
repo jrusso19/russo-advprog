ea046a356f92a974a3338e8c01e7a7d3a5676a6a
jrusso19 <31862015+jrusso19@users.noreply.github.com>
2018-02-02 14:52:06 -0500

    Add files via upload

--- connect4.py
+++ connect4.py
@@ -15,7 +15,7 @@ def main():
         while p1ok == 1:
             player1move = raw_input("PLAYER 1 (X): ")
             if player1move in ["1","2","3","4","5","6","7"]:
-                print "ok"
+                print("ok")
                 if player1move == 1:
                     column1.append(X)
                 elif player1move == 2:
@@ -32,13 +32,13 @@ def main():
                     column7.append(X)
                 p1ok = 2
             else:
-                print "please input a number from 1 to 7."
+                print("please input a number from 1 to 7.")
         p1ok = 1
-        print board(column1,column2,column3,column4,column5,column6,column7)
+        board(column1,column2,column3,column4,column5,column6,column7)
         while p2ok == 1:
             player2move = raw_input("PLAYER 2 (O): ")
             if player2move in ["1","2","3","4","5","6","7"]:
-                print "ok"
+                print ("ok")
                 if player2move == 1:
                     column1.append(O)
                 elif player2move == 2:
@@ -55,6 +55,18 @@ def main():
                     column7.append(O)
                 p2ok = 2
             else:
-                print "please input a number from 1 to 7."
+                print ("please input a number from 1 to 7.")
                 p2ok = 1
         p2ok = 1
+
+def board(column1,column2,column3,column4,column5,column6,column7):
+    for i in range (7):
+        column1.append("-")
+        column2.append("-")
+        column3.append("-")
+        column4.append("-")
+        column5.append("-")
+        column6.append("-")
+        column7.append("-")
+    for i in range (6):
+        print (column1[i-1], column2[i-1], column3[i-1], column4[i-1], column5[i-1], column6[i-1], column7[i-1])
