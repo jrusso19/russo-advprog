3131a3ce361f79df66fa065e4b84f55ad0cca996
jrusso19 <31862015+jrusso19@users.noreply.github.com>
2018-04-30 14:20:16 -0400

    Add files via upload

---
+++ goldbach.py
@@ -0,0 +1,28 @@
+def main():
+  for i in
+
+
+def listofprimes(n):
+    for i in range (2,n):
+
+
+def listprimes(n):
+    limit = n+1
+    primes = dict()
+    fullprimes = ()
+    for i in range(2, limitn): primes[i] = True
+
+    for i in primes:
+        if True:
+            fullprimes.append(i)
+
+
+
+print primes_sieve1(2000000)
+
+
+        factors = range(i,limitn, i)
+        for f in factors[1:]:
+            primes[f] = False
+    return [i for i in primes if primes[i]==True]
+    
